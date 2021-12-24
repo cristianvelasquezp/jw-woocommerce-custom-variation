aad801e10a473a490a455159b73e07adc217892f
Cristian Velasquez <cristianvelasquezp@gmail.com>
2021-12-24 13:48:07 -0600

    Create plugin JS Woocommerce Custom Variation

---
+++ js-woocommerce-variation-custom.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: JW Woocommerce Custom Variation
+Plugin URI: https://github.com/cristianvelasquezp/jw-woocommerce-custom-variation
+Description:
+Version: 1.0
+Author: Cristian Velasquez
+Author URI: https://github.com/cristianvelasquezp/
+License: GPLv2
+*/
